7639a791eb52704b5fb814e167a5ef663fe6e7d7
maximbaraliuc <m.baraliuc@gmail.com>
2022-04-19 15:46:38 +0200

    Add website structure

--- main-script.js
+++ main-script.js
@@ -1,3 +1,7 @@
+//
+//
+// Burger Menu Setup
+
 let burgerMenu = document.querySelector(".burger");
 let changeBurger = function () {
   if (burgerMenu.classList.contains("burger-inactive")) {
@@ -7,3 +11,22 @@ let changeBurger = function () {
   }
 };
 burgerMenu.addEventListener("click", changeBurger);
+
+//
+//
+// Clicked menu buttons to remain active if not clicked elsewhere
+
+let menuLinks = document.querySelectorAll(".off");
+// menuLinks.forEach((text) => console.log(text));
+// menuLinks.forEach((text) => (text.className = "on"));
+// menuLinks.forEach((text) => console.log(text));
+
+let keepActive = function () {
+  menuLinks.forEach((item) => item.classList.replace("on", "off"));
+  if (this.classList.contains("off")) {
+    this.classList.replace("off", "on");
+  }
+  console.log(this);
+};
+
+menuLinks.forEach((item) => item.addEventListener("click", keepActive));
