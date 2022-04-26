13fca07939985358a19db1ee1e56e2531093ad3d
maximbaraliuc <m.baraliuc@gmail.com>
2022-04-26 09:06:01 +0200

    Change the search bar

--- main-script.js
+++ main-script.js
@@ -86,25 +86,27 @@ window.addEventListener("resize", widthHeader);
 // Searchbar
 // ===========================================================================
 
-let searchButton = document.querySelector(".js-search-container");
-let searchArea = document.querySelector("#js-site-search");
+let searchForm = document.querySelector(".js-search-container");
+let closeButton = document.querySelector(".js-close-button");
+let searchButton = document.querySelector(".js-search-button");
 
-let searchIconChange = function () {
-  if (searchButton.classList.contains("js-search-inactive")) {
-    searchButton.classList.replace("js-search-inactive", "js-search-active");
+let searchInputActive = function (e) {
+  if (searchForm.classList.contains("js-search-inactive")) {
+    searchForm.classList.replace("js-search-inactive", "js-search-active");
     console.log("search active");
-  } else {
-    searchButton.classList.replace("js-search-active", "js-search-inactive");
+  }
+};
+let closeSearch = function (e) {
+  e.stopPropagation();
+  if (searchForm.classList.contains("js-search-active")) {
+    searchForm.classList.replace("js-search-active", "js-search-inactive");
     console.log("search inactive");
   }
 };
 
-let focusedSearch = function (e) {
-  e.stopPropagation();
-};
-
-searchButton.addEventListener("click", searchIconChange);
-searchArea.addEventListener("click", focusedSearch);
+closeButton.addEventListener("click", closeSearch);
+searchButton.addEventListener("click", closeSearch);
+searchForm.addEventListener("click", searchInputActive);
 
 // ===========================================================================
 // ...
