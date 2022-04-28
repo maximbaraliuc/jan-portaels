e7c43817da0e2cfed5cd43b41ccc72d665c2b00a
maximbaraliuc <m.baraliuc@gmail.com>
2022-04-28 16:12:02 +0200

    Start to experiment with open graph protocol

--- breadcrumbs/breadcrumbs.js
+++ breadcrumbs/breadcrumbs.js
@@ -1,3 +1,13 @@
 "use strict";
 let linkString =
-  "https://www.theguardian.com/technology/2022/apr/28/measles-cases-surge-nearly-80-in-wake-of-covid-chaos-with-fears-other-diseases-could-follow";
+  "https://www.theguardian.com/technology/measles-cases-surge-nearly-80-in-wake-of-covid-chaos-with-fears-other-diseases-could-follow";
+
+if (linkString.startsWith("https://")) {
+  linkString = linkString.slice(8);
+  console.log(linkString);
+}
+
+let stringList = [];
+stringList = linkString.split("/");
+stringList.shift();
+console.log(stringList);
