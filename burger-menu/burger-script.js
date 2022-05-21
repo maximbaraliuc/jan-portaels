addbc618757f269f60aa02eef3876e647af7ecf8
maximbaraliuc <m.baraliuc@gmail.com>
2022-05-21 02:10:24 +0200

    Clean up obsolete files

--- burger-menu/burger-script.js
+++
@@ -1,9 +0,0 @@
-let burgerMenu = document.querySelector(".burger-menu");
-let changeBurger = function () {
-  if (burgerMenu.classList.contains("burger-inactive")) {
-    burgerMenu.classList.replace("burger-inactive", "burger-active");
-  } else {
-    burgerMenu.classList.replace("burger-active", "burger-inactive");
-  }
-};
-burgerMenu.addEventListener("click", changeBurger);
--- reference-all-pages/nav.js
+++
@@ -1,8 +0,0 @@
-fetch("../reference-all-pages/nav.html")
-  .then((res) => res.text())
-  .then((text) => {
-    let oldElem = document.querySelector("#replace_with_navbar");
-    let newElem = document.createElement("header");
-    newElem.innerHTML = text;
-    oldElem.parentNode.replaceChild(newElem, oldElem);
-  });
